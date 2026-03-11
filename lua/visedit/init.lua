84d5fc68bb47ae4b0363724c70e8f46bc6860c3c
Lana <lananon1807@gmail.com>
2026-03-11 18:58:20 +0100

    attempt at adding config for visual movement

--- lua/visedit/init.lua
+++ lua/visedit/init.lua
@@ -21,20 +21,17 @@ function M.setup(opts)
     vim.keymap.set("v", "k", "<Esc>gk")
     vim.keymap.set("v", "l", "<Esc>l")
 
-    vim.keymap.set("n", "w", function () fn.next_word(vim.v.count1) end)
-    vim.keymap.set("v", "w", function () fn.next_word(vim.v.count1) end)
-    vim.keymap.set("v", "W", "w")
-    vim.keymap.set("n", "W", "vw")
+    if opts.use_vis_movement then
+        vim.keymap.set("n", "w", function () fn.next_word(vim.v.count1) end)
+        vim.keymap.set("v", "w", function () fn.next_word(vim.v.count1) end)
+        vim.keymap.set("v", "W", "w")
+        vim.keymap.set("n", "W", "vw")
 
-    vim.keymap.set("n", "b", function () fn.prev_word(vim.v.count1) end)
-    vim.keymap.set("v", "b", function () fn.prev_word(vim.v.count1) end)
-    vim.keymap.set("v", "B", "b")
-    vim.keymap.set("n", "B", "vb")
-
-    vim.keymap.set("n", "e", "viw")
-    vim.keymap.set("v", "e", "<Esc>viw")
-    vim.keymap.set("n", "E", "viw")
-    vim.keymap.set("v", "E", "iw")
+        vim.keymap.set("n", "b", function () fn.prev_word(vim.v.count1) end)
+        vim.keymap.set("v", "b", function () fn.prev_word(vim.v.count1) end)
+        vim.keymap.set("v", "B", "b")
+        vim.keymap.set("n", "B", "vb")
+    end
 
     vim.keymap.set("n", "V", function () fn.select_line(vim.v.count1) end)
     vim.keymap.set("v", "V", function () fn.select_line(vim.v.count1) end)
